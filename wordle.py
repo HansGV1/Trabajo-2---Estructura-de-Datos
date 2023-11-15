8488ab7112ba82577111a6911ff37f2fe77e8ceb
Hans García <82074617+HansGV1@users.noreply.github.com>
2023-11-15 10:05:53 -0500

    Create wordle.py

---
+++ wordle.py
@@ -0,0 +1,14 @@
+# Trabajo 1 Estructura de Datos
+#
+# Wordle Game
+#
+# Integrantes:
+# + Hans Guillermo García Vargas - hggarciag@unal.edu.co
+# + Jose Manuel Molina Vásquez  - josemoloinav@unal.edu.co
+# + Juan Jose Alzate Rojas - jalzatero@unal.edu.co
+# + Fabian Andres Chiran Guevara - fchirang@unal.edu.co
+#
+# Universidad Nacional de Colombia
+#
+#
+
