55733cd8f2bf8aacab820c49c4e1122a7cb56ec4
AlzateJJ <jalzatero@unal.edu.co>
2023-11-16 00:17:28 -0500

    implementación set() - Letra

--- wordle.py
+++ wordle.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from collections import deque
 
-a = "colo"
+a = "mano"
 long = len(a)
 
 def iniciarjuego():
@@ -27,14 +27,42 @@ def iniciarjuego():
     entry.focus_set()
     entry.pack(side='left', padx=10)
     z = 55 
+
+    class Letra:
+        def __init__(self, letra):
+            self.letra = letra
+            self.n_repeticiones = 1
+
+        def incrementar_repeticiones(self):
+            self.n_repeticiones += 1
+
+    # Función para agregar o actualizar una letra en el conjunto
+    def agregar_letra(conjunto_letras, letra):
+        # Buscar la letra en el conjunto
+        letra_existente = next((l for l in conjunto_letras if l.letra == letra), None)
+
+        # Si la letra ya existe, incrementar las repeticiones
+        if letra_existente:
+            letra_existente.incrementar_repeticiones()
+        else:
+            # Si no existe, crear una nueva instancia de Letra
+            nueva_letra = Letra(letra)
+            # Agregar la nueva letra al conjunto
+            conjunto_letras.add(nueva_letra)
+
     def asignar_colores_palabra(palabra_escogida):
         global a # a es la palabra correcta
-        conj = set(a)
+
+        conj_letras_palabra_correcta = set()
+        for letra in a:
+            agregar_letra(conj_letras_palabra_correcta, letra)
+
+        #conj = set(a) # se debe implementar grafos o diccionarios para casos de letras repetidas
         colores = list()
         for i in range(len(palabra_escogida)):
-            if palabra_escogida[i] in conj and palabra_escogida[i] == a[i]:
+            if any(letra_obj.letra == palabra_escogida[i] for letra_obj in conj_letras_palabra_correcta) and palabra_escogida[i] == a[i]:
                 colores.append("green")
-            elif palabra_escogida[i] in conj:
+            elif any(letra_obj.letra == palabra_escogida[i] for letra_obj in conj_letras_palabra_correcta):
                 colores.append("yellow")
             else:
                 colores.append("black")
@@ -42,33 +70,16 @@ def iniciarjuego():
         
     def ingresar_intento():
         global z  # Declare z as a global variable
+
         intento = entry.get()
+
         colores = asignar_colores_palabra(intento)
-        '''if long == 4:
-            for i in range(long):
-                cv.create_text(100 + i * 130, z, text=intento[i], font=("Arial", 55), fill=colores[i])
-            z += 100
-        elif long == 5:
-            for i in range(long):
-                cv.create_text(100 + i * 104, z, text=intento[i], font=("Arial", 55), fill=colores[i])
-            z += 100
-        elif long == 6:
-            for i in range(long):
-                cv.create_text(90 + i * 87, z, text=intento[i], font=("Arial", 55), fill=colores[i])
-            z += 100
-        elif long == 7:
-            for i in range(long):
-                cv.create_text(80 + i * 74, z, text=intento[i], font=("Arial", 55), fill=colores[i])
-            z += 100
-        elif long == 8:
-            for i in range(long):
-                cv.create_text(70 + i * 65, z, text=intento[i], font=("Arial", 55), fill=colores[i])
-            z += 100'''
         
         longitudesy = [100, 100, 90, 80, 70]
         longitudes = [130, 104, 87, 74, 65]
         for i in range(long):
-            cv.create_text(longitudesy[long - 4] + i * longitudes[long - 4], z, text=intento[i], font=("Arial", 55), fill=colores[i])
+            cv.create_text(longitudesy[long - 4] + i * longitudes[long - 4], z, text=intento[i], 
+                           font=("Arial", 55), fill=colores[i])
         z += 100
         print("el intento fue", intento)
         entry.delete(0, END)
