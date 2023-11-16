bcba541b79d7fa7c7d703a278b99690874a0ae26
Fabian Andres Chiran <111092524+xFa00@users.noreply.github.com>
2023-11-16 01:57:03 -0500

    funcion ganar y perder

--- wordle.py
+++ wordle.py
@@ -4,6 +4,19 @@ from collections import deque
 
 a = "mano"
 long = len(a)
+ganados = 0
+intentos= 0
+ganador= False
+
+def crear_interfaz():
+    global interfaz,f1
+
+    f1 = Frame(interfaz, width=800, height=800, bg='grey', pady=100)
+    f1.pack(side="top")
+
+    boton_iniciar_juego = Button(f1, text='Iniciar Juego de Wordle', font=("Arial", 30),
+                             bg='yellow', command=iniciarjuego, justify="center", width=18, height=4, padx=82, pady=100)
+    boton_iniciar_juego.grid(row=0, column=0, padx=7)
 
 def iniciarjuego():
     global entry, cv, z
@@ -69,7 +82,7 @@ def iniciarjuego():
         return colores
         
     def ingresar_intento():
-        global z  # Declare z as a global variable
+        global z,ganados,intentos  # Declare z as a global variable
 
         intento = entry.get()
 
@@ -81,10 +94,84 @@ def iniciarjuego():
             cv.create_text(longitudesy[long - 4] + i * longitudes[long - 4], z, text=intento[i], 
                            font=("Arial", 55), fill=colores[i])
         z += 100
-        print("el intento fue", intento)
+        intentos+=1
+        print("el intento fue el numero", intentos)
         entry.delete(0, END)
+        
+        
+        # Verificar si el usuario ha adivinado la palabra
+        if intento == a:
+            ganador = True
+            informar_ganador()
+            ganados+=1
+        elif intentos == 6 :
+            informar_perdedor()
+            
 
     ttk.Button(input_frame, text="Ingresar intento", width=20, command=ingresar_intento).pack(side='left', pady=0, padx=10)
+
+def informar_ganador():
+    global interfaz,intentos
+
+    # Mensaje que se mostrará en la interfaz
+    mensaje_ganador = "¡Felicidades! Has adivinado la palabra."
+
+    # Eliminar los widgets existentes en la interfaz
+    for widget in interfaz.winfo_children():
+        widget.destroy()
+
+    # Crear un nuevo marco para mostrar el mensaje y el botón
+    marco_ganador = Frame(interfaz, bg='grey')
+    marco_ganador.pack(expand=True)
+
+    # Etiqueta para mostrar el mensaje de ganador
+    etiqueta_ganador = Label(marco_ganador, text=mensaje_ganador, font=("Arial", 24), bg='grey', pady=20)
+    etiqueta_ganador.pack()
+
+    # Botón para reiniciar el juego
+    boton_reiniciar = Button(marco_ganador, text='Reiniciar', font=("Arial", 20), command=reiniciar_juego)
+    boton_reiniciar.pack(pady=20)
+
+    ganador= True 
+    intentos=0
+
+def informar_perdedor():
+    global interfaz
+
+    # Mensaje que se mostrará en la interfaz
+    mensaje_perdedor = ("Has perdido, la palabra era: ",a)
+
+    # Eliminar los widgets existentes en la interfaz
+    for widget in interfaz.winfo_children():
+        widget.destroy()
+
+    # Crear un nuevo marco para mostrar el mensaje y el botón
+    marco_perdedor = Frame(interfaz, bg='grey')
+    marco_perdedor.pack(expand=True)
+
+    # Etiqueta para mostrar el mensaje de perdedor
+    etiqueta_perdedor = Label(marco_perdedor, text=mensaje_perdedor, font=("Arial", 24), bg='grey', pady=20)
+    etiqueta_perdedor.pack()
+
+    # Botón para reiniciar el juego
+    boton_reiniciar = Button(marco_perdedor, text='Reiniciar', font=("Arial", 20), command=reiniciar_juego)
+    boton_reiniciar.pack(pady=20)
+
+
+def reiniciar_juego():
+    global interfaz, ganador,intentos
+
+    # Destruir widgets adicionales
+    for widget in interfaz.winfo_children():
+        widget.destroy()
+
+    # Volver al inicio del código y crear la interfaz inicial
+    crear_interfaz()
+
+    ganador = False
+    print(ganados)
+    intentos=0
+    
 
 interfaz = Tk()
 interfaz.title('Wordle')
@@ -92,14 +179,10 @@ interfaz.geometry('600x690')
 interfaz.config(bg='grey')
 interfaz.resizable(width=False, height=False)
 
-f1 = Frame(interfaz, width=800, height=800, bg='grey', pady=100)
-f1.pack(side="top")
-
-boton_iniciar_juego = Button(f1, text='Iniciar Juego de Wordle', font=("Arial", 30),
-                             bg='yellow', command=iniciarjuego, justify="center", width=18, height=4, padx=82, pady=100)
-boton_iniciar_juego.grid(row=0, column=0, padx=7)
+crear_interfaz()
 
 
 
 interfaz.update()
 interfaz.mainloop()
+
