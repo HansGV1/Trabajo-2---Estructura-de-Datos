e6b8fbd5c388ae6e0a4840ab71c36a4bac46a9d4
Hans García <hansgarcia@ibm.com>
2023-11-16 20:01:28 -0500

    Funcionalidad trie, combinación tkinter

--- wordle.py
+++ wordle.py
@@ -1,13 +1,3 @@
-from tkinter import *
-from tkinter import ttk
-from collections import deque
-
-a = "mano"
-long = len(a)
-ganados = 0
-intentos= 0
-ganador= False
-
 # Trabajo 1 Estructura de Datos
 #
 # Wordle Game
@@ -24,6 +14,9 @@ ganador= False
 
 from collections import deque
 import random
+from tkinter import *
+from tkinter import ttk
+from collections import deque
 
 class TrieNode:
     def __init__(self):
@@ -43,7 +36,7 @@ class Trie:
             if char not in node.children:
                 node.children[char] = TrieNode()
             node = node.children[char]
-            node.word_count += 1
+            node.word_count += 1  # 
         node.is_end_of_word = True
         self.word_list.append(word)
 
@@ -78,15 +71,13 @@ def inputDificultad():
 def trie():
     Lemario = ['pipeline', 'beards', 'hapiness', 'dessert', 'maybe', 'puzzle', 'same', 'showroom', 'wafer', 'dracula', 'voor', 'noodle', 'reshape', 'stigma', 'dwellers', 'gripe', 'keyrings', 'dorothea', 'hecht', 'dubuque', 'widening', 'metro', 'crichton', 'wenn', 'pamphlet', 'aarhus', 'convoy', 'stone', 'fail', 'roommate', 'moro', 'paint', 'viacom', 'ebayer', 'glace', 'pension', 'bathtub', 'snot', 'vitesse', 'alonso', 'precio', 'balm', 'pointer', 'clarins', 'tendency', 'hematite', 'bromide', 'boat', 'sexually', 'aguirre', 'tarp', 'prevail', 'charles', 'comedy', 'nearing', 'freeport', 'faerie', 'silicate', 'confined', 'equinox', 'raitt', 'crud', 'bbci', 'estrogen', 'stewards', 'equate', 'fountain', 'dara', 'eugene', 'kessler', 'titleist', 'scherer', 'nutty', 'fairs', 'licences', 'osage', 'traders', 'args', 'jodie', 'samantha', 'siege', 'though', 'unable', 'lesbicas', 'upper', 'romero', 'martel', 'calvin', 'sumitomo', 'tube', 'brunson', 'monarchs', 'pedersen', 'stables', 'matte', 'couture', 'latino', 'novella', 'strat', 'ashburn', 'thirds', 'bakker', 'rotten', 'weaker', 'kylie', 'orkney', 'cheater', 'falmouth', 'gregorio', 'lampoon', 'collect', 'racer', 'faster', 'blick', 'prone', 'destroy', 'outbound', 'beet', 'shortage', 'hiphop', 'janice', 'approves', 'whats', 'neve', 'taboo', 'harms', 'closeups', 'ruger', 'cherie', 'mondo', 'fractals', 'burgers', 'sarasota', 'batten', 'colman', 'alpaca', 'owls', 'stamina', 'tower', 'textile', 'keele', 'pumpkins', 'come', 'robeson', 'testy', 'pols', 'equifax', 'contre', 'fdisk', 'lockout', 'failed', 'debadmin', 'oranges', 'picket', 'hooks', 'maryann', 'nieuwe', 'poisons', 'schrieb', 'cfnm', 'alienate', 'incoming', 'vila', 'pixma', 'reisen', 'stanford', 'carnal', 'inch', 'sara', 'alexia', 'cadence', 'wished', 'angebote', 'homage', 'capacity', 'torque', 'diflucan', 'spout', 'oyster', 'budget', 'llcs', 'driving', 'kruger', 'virgil', 'baton', 'motoring', 'leyland', 'mancha', 'wept', 'cousin', 'wartime', 'donut', 'lunatic', 'niagara', 'religion', 'heart', 'lola', 'loti', 'mildura', 'annals', 'barge', 'phases', 'debts', 'pippin', 'pollock', 'snails', 'chip', 'adjunct', 'dementia', 'elgin', 'bridger', 'consoles', 'ignored', 'hausa', 'arranges', 'worldcat', 'mowers', 'whenever', 'oddities', 'required', 'writing', 'executed', 'falcons', 'favors', 'caliber', 'hernando', 'older', 'shandong', 'endian', 'knopf', 'clever', 'iogear', 'pyle', 'ruthless', 'reign', 'rino', 'pupil', 'double', 'infosec', 'assessed', 'marketed', 'shakers', 'optimise', 'sesso', 'taro', 'ambien', 'foils', 'withdrew', 'ignite', 'pale', 'magnet', 'deering', 'alltel', 'torsion', 'plas', 'modeling', 'high', 'cosco', 'casts', 'token', 'scenic', 'rancho', 'forde', 'galore', 'allure', 'beverly', 'wooded', 'stabbing', 'klamath', 'ajmer', 'huts', 'trackers', 'rundown', 'mandated', 'junebug', 'surveys', 'sociales', 'stinky', 'sues', 'fichier', 'shiraz', 'cast', 'hansard', 'alex', 'scope', 'blinked', 'searle', 'westward', 'panic', 'soyo', 'freakin', 'built', 'reusing', 'harris', 'warmest', 'meters', 'lsat', 'memory', 'chord', 'meanings', 'serenade', 'hurley', 'mammals', 'mika', 'isola', 'devotee', 'zambezi', 'frosting', 'praises', 'myself', 'specific', 'brigade', 'gradient', 'diary', 'original', 'myer', 'inquired', 'forrest', 'pantech', 'laura', 'came', 'watcher', 'philip', 'canning', 'dscp', 'nothing', 'clemson', 'parallel', 'chard', 'partie', 'hyundai', 'pies', 'picked', 'repaired', 'tucows', 'milk', 'xpdf', 'rogues', 'fleece', 'agri', 'draw', 'doped', 'passat', 'defines', 'howling', 'slimming', 'dealer', 'talkin', 'imposing', 'techweb', 'anode', 'paragon', 'hygiene', 'watches', 'postmark', 'chatter', 'minidisc', 'michaels', 'barlow', 'jolt', 'valance', 'roselle', 'gaining', 'yorke', 'rotate', 'wang', 'lethal', 'ordering', 'toggle', 'deluxe', 'parting', 'lott', 'autonomy', 'ladybird', 'skimage', 'dominion', 'stingy', 'towns', 'rage', 'trixie', 'helios', 'militia', 'mainpage', 'oxygen', 'stis', 'rowland', 'faction', 'mifflin', 'cushion', 'vesta', 'variable', 'henning', 'louise', 'yogurt', 'howtos', 'yashica', 'bulk', 'torpedo', 'hosting', 'mise', 'bouts', 'rusk', 'appease', 'inode', 'goodness', 'kardon', 'mohawk', 'catching', 'jossey', 'musings', 'lois', 'skating', 'reims', 'koji', 'fast', 'puma', 'quiver', 'absence', 'equip', 'lebron', 'rmdir', 'hodge', 'realtor', 'winer', 'bancroft', 'rooter', 'bought', 'sophos', 'bistro', 'anza', 'simcoe', 'tactic', 'tucson', 'citroen', 'kazan', 'centre', 'meetups', 'duracell', 'sprache', 'camargo', 'frazier', 'pkgsrc', 'infill', 'joining', 'meine', 'trinket', 'boys', 'palm', 'cages', 'handling', 'junkyard', 'inner', 'erode', 'plow', 'itat', 'lawless', 'vicodin', 'pebble', 'info', 'rubles', 'looping', 'mine', 'sharks', 'dishes', 'jewelry', 'connects', 'wayne', 'tracking', 'mint', 'elders', 'berne', 'pour', 'heyday', 'bally', 'sucking', 'longing', 'awake', 'grantor', 'custom', 'martyr', 'baku', 'stade', 'balloon', 'hardwood', 'corps', 'parkside', 'astor', 'gbps', 'helsing', 'navi', 'warlock', 'shrunk', 'make', 'bossa', 'abusing', 'pleading', 'sheriffs', 'zyban', 'dumped', 'sheikh']
     trie = Trie()
-    
     for palabra in Lemario:
         trie.insert(palabra)
-         
     return trie
 
 
-def generadorPalabra(trie):
-    palabraDelDia = trie.retrieve_random_word(inputDificultad())
+def generadorPalabra(trie, dificultad):
+    palabraDelDia = trie.retrieve_random_word(dificultad)
     return palabraDelDia
 
 def inputUsuario(trie):
@@ -100,6 +91,11 @@ def inputUsuario(trie):
     else:
         print("Incorrecto! Has agotado tus intentos. Hasta la próxima!")
 
+a = generadorPalabra(trie(), inputDificultad())
+long = len(a)
+ganados = 0
+intentos= 0
+ganador= False
 
 def crear_interfaz():
     global interfaz,f1
@@ -125,14 +121,18 @@ def iniciarjuego():
         for j in range(25, 575, 100):
             cv.create_rectangle(i, j, i + int(550 / long), j + 70, fill='white')
 
-
     input_frame = Frame(interfaz, bg='grey')
     input_frame.pack(side='top', pady=10)
 
-    entry = Entry(input_frame, width=10, justify="left", font=["Arial", 30])
+    def validate_input(char, current_text):
+        return char.isalpha() or char == ""
+
+    vcmd = (interfaz.register(validate_input), '%S', '%P')
+
+    entry = Entry(input_frame, width=10, justify="left", font=["Arial", 30], validate="key", validatecommand=vcmd)
     entry.focus_set()
     entry.pack(side='left', padx=10)
-    z = 55 
+    z = 55
 
     class Letra:
         def __init__(self, letra):
@@ -188,7 +188,7 @@ def iniciarjuego():
                            font=("Arial", 55), fill=colores[i])
         z += 100
         intentos+=1
-        print("el intento fue el numero", intentos)
+        print("El intento fue el numero: ", intentos)
         entry.delete(0, END)
         
         
